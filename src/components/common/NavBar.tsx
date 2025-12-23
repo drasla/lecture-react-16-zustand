612615cc98308f53ddab641c46d8c026db919d31
Tae.Garam <drasla@naver.com>
2025-12-23 12:08:52 +0900

    useThemeStore 통한 테마 작성 완료

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,7 +1,18 @@
 import { twMerge } from "tailwind-merge";
 import { Link } from "react-router";
+import { useThemeStore } from "../../stores/useThemeStore.ts";
+import { FiMoon, FiSun } from "react-icons/fi";
+import { useEffect } from "react";
 
 function NavBar() {
+    const { isDark, toggleTheme } = useThemeStore();
+
+    useEffect(() => {
+        const html = document.documentElement;
+        if (isDark) html.classList.add("dark");
+        else html.classList.remove("dark");
+    }, [isDark]);
+
     // sticky : 스크롤을 내리더라도 그 자리에 계속 존재하라는 tailwind 클래스
     // top-0 : 부모를 기준 잡아, 부모 상단에서 0px 위치에 존재
     // z-index : z-index 레이어
@@ -22,7 +33,16 @@ function NavBar() {
                 )}>
                 MyTodo
             </Link>
-            <div>
+            <div className={twMerge(["flex", "items-center", "gap-2"])}>
+                <button
+                    onClick={toggleTheme}
+                    className={twMerge(
+                        ["p-2"],
+                        ["rounded-full", "transition"],
+                        ["hover:bg-divider"],
+                    )}>
+                    {isDark ? <FiSun size={20} /> : <FiMoon size={20} />}
+                </button>
                 <Link
                     to={"/sign-in"}
                     className={twMerge(
@@ -38,7 +58,7 @@ function NavBar() {
                         ["px-3", "py-2"],
                         ["text-sm", "font-medium", "text-primary-contrastText"],
                         ["bg-primary-main", "rounded-md", "hover:bg-primary-dark"],
-                        "transition-colors"
+                        "transition-colors",
                     )}>
                     Sign Up
                 </Link>
